69d50f5d3eeab6621a8bcc3e90160f7073c9d483
ruden4 <ruden4@gmail.com>
2023-08-08 17:19:36 +0300

    itWorks!

---
+++ src/cat-api.js
@@ -0,0 +1,27 @@
+const API_KEY = 'live_CfrllrdCWOagVVd0syszXTyG8ZIWt9i8Spe8uOpHnyAKUk0Q7plUoMGCZ2AQAkMF';
+
+export function fetchBreeds() {
+    return fetch(`https://api.thecatapi.com/v1/breeds?api_key=${API_KEY}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        throw new Error('Failed breeds fetch');
+      });
+  }
+
+  export function fetchCatByBreed(breedId) {  
+    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${API_KEY}`)
+      .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        throw new Error('Failed cat API fetch');
+      });
+  }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,47 @@
-// {/* <select name="select">
-//   <!--Supplement an id here instead of using 'name'-->
-//   <option value="value1">Значение 1</option>
-//   <option value="value2" selected>Значение 2</option>
-//   <option value="value3">Значение 3</option>
-// </select> */}
-// https://api.thecatapi.com/v1/breeds
-const API_KEY = 'live_CfrllrdCWOagVVd0syszXTyG8ZIWt9i8Spe8uOpHnyAKUk0Q7plUoMGCZ2AQAkMF';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
 const breedOptionsEl = document.querySelector('.breed-select');
 const loaderEl = document.querySelector('.loader');
 const errorEl = document.querySelector('.error');
 const catInfoEl = document.querySelector('.cat-info');
 
-let breadNumb = 0;
-const fetchCatsBreadOptions = fetch(`https://api.thecatapi.com/v1/breeds?api_key=${API_KEY}`)
-.then(response => { 
-    if (!response.ok) {
-        throw new Error(response.status)
-    } 
-    return response.json();
-})
-.then(data => data.map(({name, id}) => `<option value="${id}">${name}</option>`).join(''))
-.catch(error => console.log(error));
 
-console.log(fetchCatsBreadOptions)
+function populateBreedOptions() {
+  fetchBreeds()
+    .then(breeds => {
+      const optionsHTML = breeds
+        .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
+        .join('');
+      breedOptionsEl.innerHTML = optionsHTML;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+populateBreedOptions();
+
+breedOptionsEl.addEventListener('change', onChangeOption)
+
+function onChangeOption (e) {
+    
+    const currentCat = e.target.value;
+    
+    fetchCatByBreed(currentCat).then(cat => {
+        console.log(cat[0].breeds[0].temperament)
+        const catImg = cat[0].url;
+        const catName = cat[0].breeds[0].name;
+        const catDescr = cat[0].breeds[0].description;
+        const catTemperament = cat[0].breeds[0].temperament;
+        
+        const catMarkup =`
+        <img src="${catImg}" alt="" height="300">
+      <h2>${catName}</h2>
+      <p>${catDescr}</p>
+      <p>Temperament: ${catTemperament}</p>
+        `
+        catInfoEl.innerHTML = catMarkup;
+    }).catch(error => {
+        console.error(error);
+    });
+    
+} 
